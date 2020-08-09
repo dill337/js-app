20366527421722596af3ea10c2fe2382d4a3c208
dill337 <64544519+dill337@users.noreply.github.com>
2020-08-08 20:42:02 -0500

    Added favorite food

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
-var myName = 'Fred';
-document.write(myName);
-
 var size = 100;
 var doubleSize = size * 2;
 var minSize = (doubleSize * 2) - (size / 2);
 document.write(minSize);
 
 alert('Hello World')
+
+var favoriteFood = 'Steak';
+document.write(favoriteFood)

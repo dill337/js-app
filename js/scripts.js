a8becf635233057b220a4c6514fababec1c31ef3
dill337 <64544519+dill337@users.noreply.github.com>
2020-08-10 14:16:38 -0500

    Added pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,6 @@
-var size = 100;
-var doubleSize = size * 2;
-var minSize = (doubleSize * 2) - (size / 2);
-document.write(minSize);
-
-alert('Hello World')
-
-var favoriteFood = 'Steak';
-document.write(favoriteFood)
+pokemonList [
+  {name: 'Bulbasaur', height: .7, weight: 6.9, creatureType:['Grass', 'Poison']}
+  {name: 'Arbok', height: 3.5, weight: 65, creatureType: 'Poison'}
+  {name: 'Charizard', height: 1.7, weight: 90.5, creatureType: ['Fire', 'Flying']}
+  {name: 'Beedrill', height: 1, weight: 29.5, creatureType: ['Bug', 'Poison']}
+]

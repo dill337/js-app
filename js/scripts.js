e59c1a549aee88b56498948da00d8644070224c3
dill337 <64544519+dill337@users.noreply.github.com>
2020-08-12 11:36:28 -0500

    added loop for pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,33 @@
-pokemonList [
-  {name: 'Bulbasaur', height: .7, weight: 6.9, creatureType:['Grass', 'Poison']}
-  {name: 'Arbok', height: 3.5, weight: 65, creatureType: 'Poison'}
-  {name: 'Charizard', height: 1.7, weight: 90.5, creatureType: ['Fire', 'Flying']}
-  {name: 'Beedrill', height: 1, weight: 29.5, creatureType: ['Bug', 'Poison']}
-]
+var pokemonList = [
+  {
+  creatureName: 'Bulbasaur',
+  creatureHeight: .7,
+  creatureWeight: 6.9,
+  creatureType:['Grass', 'Poison']
+  },
+  {
+  creatureName: 'Arbok',
+  creatureHeight: 3.5,
+  creatureWeight: 65,
+  creatureType: 'Poison'
+  },
+  {
+  creatureName: 'Charizard',
+  creatureHeight: 1.7,
+  creatureWeight: 90.5,
+  creatureType: ['Fire', 'Flying']
+  },
+  {
+  creatureName: 'Beedrill',
+  creatureHeight: 1,
+  creatureWeight: 29.5,
+  creatureType: ['Bug', 'Poison']
+  }
+];
+
+for (var i = 0; i < pokemonList.length; i++) {
+  document.write('<h2>' + pokemonList[i].creatureName + ' </h2>' + ' Height: ' + pokemonList[i].creatureHeight)
+ if (pokemonList[i].creatureHeight >= 3.00) {
+  document.write(' (That\'s pretty big)')
+  }
+}

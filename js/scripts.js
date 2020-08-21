4c6c1f1bbee805419b4e5347fdb787d1b5297f55
dill337 <64544519+dill337@users.noreply.github.com>
2020-08-21 16:55:06 -0500

    Updated from bogus app

--- js/scripts.js
+++ js/scripts.js
@@ -31,20 +31,50 @@ var pokemonList = [
   }
 
   function add(pokemon) {
+    if (typeof pokemon !== 'object');
+    return
+    const knownKeys = ['creatureName', 'creatureWeight', 'creatureType', 'creatureHeight']
+   for (const key in Object.keys(pokemon)) {
+       if (!knownKeys.includes(key)) {
+          return
+       }
+   }
     pokemonList.push(pokemon);
   }
 
+  function showDetails(pokemon) {
+    console.log(pokemon)
+  };
+
+
+  function addListItem(pokemon) {
+    var poke = document.querySelector('.pokemon_list');
+    var listItem = document.createElement('li');
+    var button = document.createElement('button');
+    button.innerText = pokemon.creatureName;
+    button.classList.add('.button_color');
+    listItem.appendChild(button);
+    poke.appendChild(listItem);
+    button.addEventListener(click, function (event) {
+      showDetails(pokemon);
+    });
+  };
+
+
   return {
     getAll: getAll,
-    add: add
+    add: add,
+    addListItem: addListItem
   }
 })();
 
-console.log( pokemonRepository.getAll() );
-
-pokemonRepository.getAll().forEach(function(list)  {
-  document.write('<h2>' + list.creatureName + ' </h2>' + ' Height: ' + list.creatureHeight)
- if (list.creatureHeight >= 3.00) {
-  document.write(' (That\'s pretty big)')
-  }
+pokemonRepository.getAll().forEach(function(pokemon)  {
+  console.log(pokemon.creatureName);
+  var poke = document.querySelector('.pokemon_list');
+  var listItem = document.createElement('li');
+  var button = document.createElement('button');
+  button.innerText = pokemon.creatureName;
+  button.classList.add('.button_color');
+  listItem.appendChild(button);
+  poke.appendChild(listItem);
 });

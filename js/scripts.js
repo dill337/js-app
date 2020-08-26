e98854a9b5a140137ed89d58e523204dd49a77f8
dill337 <64544519+dill337@users.noreply.github.com>
2020-08-26 12:33:59 -0500

    cleaned up
    got rid of old keys "creature name" etc

--- js/scripts.js
+++ js/scripts.js
@@ -5,12 +5,12 @@ var pokemonRepository = (function () {
 
 
   function add(pokemon) {
-    if (typeof pokemon !== 'object');{
+    if (typeof pokemon !== 'object'){
     return
     }
-    const knownKeys = ['creatureName', 'creatureWeight', 'creatureType', 'creatureHeight']
+    const knownKeys = ['name', 'detailsUrl']
    for (const key in Object.keys(pokemon)) {
-       if (!knownKeys.includes(key)) {
+       if (knownKeys.includes(key)) {
           return
        }
    }
@@ -24,7 +24,6 @@ var pokemonRepository = (function () {
   function showDetails(pokemon) {
     pokemonRepository.loadDetails(pokemon).then(function(){
     console.log(pokemon);
-    alert('height:' + ' ' + item.height + ' ' + 'Type: ' + ' ' + item.types);
   });
   }
 
@@ -33,7 +32,7 @@ var pokemonRepository = (function () {
     var poke = document.querySelector('.pokemon_list');
     var listItem = document.createElement('li');
     var button = document.createElement('button');
-    button.innerText = pokemon.creatureName;
+    button.innerText = pokemon.name;
     button.classList.add('.button_color');
     listItem.appendChild(button);
     poke.appendChild(listItem);

0f75fb7a70dea2c6290e0cc6bf05a2574d932ddd
dill337 <64544519+dill337@users.noreply.github.com>
2020-08-31 18:38:17 -0500

    backup js

--- js/newBogus.js
+++ js/newBogus.js
@@ -3,22 +3,44 @@ var pokemonRepository = (function () {
   var apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
 
-  function getAll(){
-    return pokemonList;
-  }
 
   function add(pokemon) {
-    if (typeof pokemon !== 'object');{
+    if (typeof pokemon !== 'object'){
     return
     }
-    const knownKeys = ['creatureName', 'creatureWeight', 'creatureType', 'creatureHeight']
+    const knownKeys = ['name', 'detailsUrl']
    for (const key in Object.keys(pokemon)) {
-       if (!knownKeys.includes(key)) {
+       if (knownKeys.includes(key)) {
           return
        }
    }
     pokemonList.push(pokemon);
   }
+
+  function getAll(){
+    return pokemonList;
+  }
+
+  function showDetails(pokemon) {
+    pokemonRepository.loadDetails(pokemon).then(function(){
+    console.log(pokemon);
+    alert('height:' + ' ' + pokemon.height + ' ' + 'Type: ' + ' ' + pokemon.types);
+  });
+  }
+
+
+  function addListItem(pokemon) {
+    var poke = document.querySelector('.pokemon_list');
+    var listItem = document.createElement('li');
+    var button = document.createElement('button');
+    button.innerText = pokemon.name;
+    button.classList.add('.button_color');
+    listItem.appendChild(button);
+    poke.appendChild(listItem);
+    button.addEventListener('click', function (event) {
+      showDetails(pokemon);
+    });
+  };
 
   function loadList() {
     return fetch(apiUrl).then(function (response) {
@@ -33,27 +55,7 @@ var pokemonRepository = (function () {
       });
     }).catch(function (e) {
       console.error(e);
-    });
-  }
-
-  function addListItem(pokemon) {
-    var buttonList = document.querySelector('.pokemon_list');
-    var listItem = document.createElement('li');
-    var button = document.createElement('button');
-    button.innerText = pokemon.creatureName;
-    button.addEventListener('click', function (event) {
-      showDetails(pokemon);
     })
-    button.classList.add('.button_color');
-    listItem.appendChild(button);
-    buttonList.appendChild(listItem);
-  };
-
-  function showDetails(pokemon) {
-    pokemonRepository.loadDetails(pokemon).then(function(){
-    console.log(pokemon);
-    alert('height:' + ' ' + item.height + ' ' + 'Type: ' + ' ' + item.types);
-  });
   }
 
   function loadDetails(item) {
@@ -67,6 +69,10 @@ var pokemonRepository = (function () {
     }).catch(function(error){
       console.error(error);
     });
+  }
+
+  function catchAll(){
+    return repository;
   }
 
   return {

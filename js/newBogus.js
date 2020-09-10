730e07cbd64dd88f7163e0189a3e0b0d5fb461d7
dill337 <64544519+dill337@users.noreply.github.com>
2020-09-09 21:19:30 -0500

    updated with bootstrap

--- js/newBogus.js
+++ js/newBogus.js
@@ -21,28 +21,86 @@ var pokemonRepository = (function () {
     return pokemonList;
   }
 
+
   function showDetails(pokemon) {
-    pokemonRepository.loadDetails(pokemon).then(function(){
-    console.log(pokemon);
-    alert('height:' + ' ' + pokemon.height + ' ' + 'Type: ' + ' ' + pokemon.types);
-  });
-  }
+    let modal = $('#modal-container');
+    let modalHeading = $('#pokemonModalLabel');
+    let modalBody = $('.modal-body');
+    modalHeading.html("");
+    modalBody.html("");
+     pokemonRepository.loadDetails(pokemon).then(function(){
+       console.log(pokemon)
+       modalHeading.text(`This is ${pokemon.name}`);
+
+       let height = $('<p></p>');
+       height.text(`Height: ${pokemon.height} meters`);
+
+       let weight = $('<p></p>');
+       weight.text(`Weight: ${pokemon.weight} lbs`)
+
+       let image = $('<img></img>');
+       image.attr('src', pokemon.imageUrl);
+
+       pokemon.types.forEach((type) => {
+         let typeElement = $('<span></span>');
+         typeElement.text(type.type.name);
+         $(modalBody).append(typeElement)
+       })
+
+       $(modalBody).append(height);
+       $(modalBody).append(weight);
+       $(modalBody).append(image);
+
+     });
+   }
+
+   function hideModal() {
+     var modal =  document.querySelector('#modal-container-is-visible');
+     modal.setAttribute('id','modal-container');
+        while(modal.firstChild){
+         modal.removeChild(modal.firstChild);
+     }
+   };
 
 
-  function addListItem(pokemon) {
-    var poke = document.querySelector('.pokemon_list');
-    var listItem = document.createElement('li');
-    var button = document.createElement('button');
-    button.innerText = pokemon.name;
-    button.classList.add('.button_color');
-    listItem.appendChild(button);
-    poke.appendChild(listItem);
-    button.addEventListener('click', function (event) {
-      showDetails(pokemon);
-    });
-  };
+$(window).on('keydown', function(e) {
+var modalContainer = $('#modal-container-is-visible');
+if (e.key === 'Escape' && modalContainer) {
+  hideModal();
+}
+});
 
-  function loadList() {
+   function addListItem(pokemon) {
+      var poke = $('.pokemon_list');
+      var listItem = $('<li></li>');
+      var button = $(`<button type="button" class="btn btn-primary" data-toggle="modal"
+      data-target="#pokemonModal">${pokemon.name}</button>`);
+      $(listItem).append(button);
+      $(poke).append(listItem);
+      $(button).on('click', function (event) {
+        showDetails(pokemon);
+
+
+      });
+    };
+
+   /*function loadList() {
+      return $.ajax({url:apiUrl}).then(function (response) {
+        return response.json();
+      }).then(function (json) {
+        forEach(let item in json.results){
+          var pokemon = {
+            name: item.name,
+            detailsUrl: item.url
+          };
+          add(pokemon);
+        }
+      }).catch(function (e) {
+        console.error(e);
+      })
+    }*/
+
+  /*function loadList() {
     return fetch(apiUrl).then(function (response) {
       return response.json();
     }).then(function (json) {
@@ -56,6 +114,22 @@ var pokemonRepository = (function () {
     }).catch(function (e) {
       console.error(e);
     })
+  }*/
+
+  function loadList() {
+    return $.ajax({url:apiUrl}).done(function (response) {
+      return JSON.stringify(response);
+    }).done(function (json) {
+      json.results.forEach(function (item) {
+        var pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        add(pokemon);
+      });
+    }).fail(function (e) {
+      console.error(e);
+    })
   }
 
   function loadDetails(item) {
@@ -65,7 +139,8 @@ var pokemonRepository = (function () {
     }).then(function(details){
       item.imageUrl = details.sprites.front_default;
       item.height = details.height;
-      item.types = Object.keys(details.types);
+      item.types = details.types;
+      item.weight = details.weight;
     }).catch(function(error){
       console.error(error);
     });
